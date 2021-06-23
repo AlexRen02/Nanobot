66909964b0f19ab36c85486bc0b19f522d992958
AlexRen02 <renalexj@gmail.com>
2021-06-23 13:01:55 -0700

    Change dice roll to function

--- nanobot.py
+++ nanobot.py
@@ -57,13 +57,55 @@ def lottery_claim(driver, last, lotto_total):
 
 # Send Chat
 def send_chat(driver, chat):
+    # Find chat box
     chat_box = driver.find_element_by_xpath('//*[@id="chat-input"]')
+    
+    # Send chat
     chat_box.send_keys(chat + Keys.ENTER)
+
+# Play Dice
+def play_dice(driver, dice_page, bet, payout):
+    my_tickets = int(driver.find_element_by_xpath('//*[@id="header_silver_count"]/span').get_attribute('innerHTML'))
+    if my_tickets > 0:
+        dice_page.click()
+        time.sleep(5)
+
+        # Dice page buttons
+        dice_bet_input = driver.find_element_by_xpath('//*[@id="dice_manual_bet"]/input')
+        dice_payout_input = driver.find_element_by_xpath('//*[@id="dice_manual_payout"]/input')
+        roll_under_button = driver.find_element_by_xpath('//*[@id="dice_manual_roll_low"]')
+        roll_over_button = driver.find_element_by_xpath('//*[@id="dice_manual_roll_high"]')
+
+        while my_tickets > 0:
+            # Send dice info
+            dice_bet_input.send_keys(Keys.CONTROL, 'a') 
+            dice_bet_input.send_keys(bet)
+            dice_payout_input.send_keys(Keys.CONTROL, 'a')
+            dice_payout_input.send_keys(payout)
+
+            # Select dice roll and roll
+            if select_roll() == 'over':
+                roll_over_button.click()
+            else:
+                roll_under_button.click()
+
+            # Get new ticket amount
+            time.sleep(1)
+            my_tickets = int(driver.find_element_by_xpath('//*[@id="header_silver_count"]/span').get_attribute('innerHTML'))
+
+# Dice roll selection
+def select_roll():
+    # Currently random selection
+    # To be changed
+    x = random.random()
+    if x < 0.5:
+       return 'over'
+    else:
+        return 'under'
 
 def main():
     # Website URL
     url = 'https://luckynano.com/?p=index'
-    dice = 'https://luckynano.com/?p=dice'
 
     # Open Chrome
     driver = webdriver.Chrome(ChromeDriverManager().install())
@@ -74,9 +116,10 @@ def main():
     # Wait for page to load
     time.sleep(1)
 
-    # Home Page Button
+    # Website Page Buttons
     home_page = driver.find_element_by_xpath('//*[@id="home_logo"]')
-
+    dice_page = driver.find_element_by_xpath('//*[@id="header_content"]/div[4]/div[2]')
+    
     # Time Variables
     last = datetime.datetime(2000, 1, 1) # Last lottery ticket collection time
 
@@ -84,7 +127,6 @@ def main():
     lotto_total = 0
 
     # Dice Roll Variables
-    my_tickets = 0
     ticket_bet = '1'
     ticket_payout = '10'
     #nano_bet = 0.001
@@ -103,42 +145,7 @@ def main():
         last, lotto_total = lottery_claim(driver, last, lotto_total)
 
         # Auto dice roll
-        my_tickets = int(driver.find_element_by_xpath('//*[@id="header_silver_count"]/span').get_attribute('innerHTML'))
-        if my_tickets > 0:
-            # Dice Roll Page
-            dice_page = driver.find_element_by_xpath('//*[@id="header_content"]/div[4]/div[2]')
-            time.sleep(1)
-
-            # Go to dice page
-            dice_page.click()
-            time.sleep(5)
-
-            # Dice page buttons
-            dice_bet_input = driver.find_element_by_xpath('//*[@id="dice_manual_bet"]/input')
-            dice_payout_input = driver.find_element_by_xpath('//*[@id="dice_manual_payout"]/input')
-            roll_under_button = driver.find_element_by_xpath('//*[@id="dice_manual_roll_low"]')
-            roll_over_button = driver.find_element_by_xpath('//*[@id="dice_manual_roll_high"]')
-
-            while my_tickets != 0:
-                time.sleep(5)
-
-                # Send dice info
-                dice_bet_input.send_keys(Keys.CONTROL, 'a') 
-                dice_bet_input.send_keys(ticket_bet)
-                dice_payout_input.send_keys(Keys.CONTROL, 'a')
-                dice_payout_input.send_keys(ticket_payout)
-
-                # Randomly select roll under or roll over
-                # To be changed
-                x = random.random()
-                if x < 0.5:
-                    roll_over_button.click()
-                else:
-                    roll_under_button.click()
-            
-                # New ticket amount
-                time.sleep(1)
-                my_tickets = int(driver.find_element_by_xpath('//*[@id="header_silver_count"]/span').get_attribute('innerHTML'))
+        play_dice(driver, dice_page, ticket_bet, ticket_payout)
 
         # Return to home page if not rolling
         time.sleep(2)
